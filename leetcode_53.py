9d49db7d1b979684b33a4f74ca4217da1403f96c
kkkkang1009 <kkkkkkkkang@nate.com>
2019-06-22 19:10:44 +0900

    Change scoring criteria

--- leetcode_53.py
+++ leetcode_53.py
@@ -1,11 +1,23 @@
 # 53. Maximum Subarray
+# Change scoring criteria - TimeOut
+# class Solution:
+#    def maxSubArray(self, nums: 'List[int]') -> 'int':
+#        max = nums[0]
+#        for i in range(0, len(nums)):
+#            sum = 0
+#            for j in range(i, len(nums)):
+#                sum += nums[j]
+#                if sum > max:
+#                    max = sum
+#        return max
+#
 class Solution:
-   def maxSubArray(self, nums: 'List[int]') -> 'int':
-       max = nums[0]
-       for i in range(0, len(nums)):
-           sum = 0
-           for j in range(i, len(nums)):
-               sum += nums[j]
-               if sum > max:
-                   max = sum
-       return max
+    def maxSubArray(self, nums: List[int]) -> int:
+        stack = []
+        list =- float('inf')
+        for i in nums:
+            if sum(stack) + i < i:
+                stack = []
+            stack.append(i)
+            list = max(list, sum(stack))
+        return list
--- leetcode_69.py
+++ leetcode_69.py
@@ -1,12 +1,24 @@
 # 69. Sqrt(x)
+# Change scoring criteria - TimeOut
+# class Solution:
+#    def mySqrt(self, x: 'int') -> 'int':
+#        answer = 0
+#        while answer <=  x:
+#            if answer*answer == x:
+#                return answer
+#            elif answer*answer > x:
+#                break
+#            else:
+#                answer += 1
+#        return answer-1
+
+
 class Solution:
-   def mySqrt(self, x: 'int') -> 'int':
-       answer = 0
-       while answer <=  x:
-           if answer*answer == x:
-               return answer
-           elif answer*answer > x:
-               break
-           else:
-               answer += 1
-       return answer-1
+    def mySqrt(self, x: 'int') -> 'int':
+        answer = 0
+        while answer <=  x:
+            if answer*answer > x:
+                break
+            else:
+                answer += 1
+        return answer-1
