1f69c99f0cf31d567970526e5561096c5288c45f
kkkkang1009 <kkkkkkkkang@nate.com>
2019-06-29 17:38:50 +0900

    112. Path Sum

---
+++ leetcode_112.py
@@ -0,0 +1,24 @@
+# 112. Path Sum
+class Solution:
+    exist = False
+    totalSum = 0
+
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        self.totalSum = sum
+        if root is None:
+            return False
+        else:
+            nowSum = 0
+            self.checkSum(root, nowSum)
+            return self.exist
+
+    def checkSum(self, root: TreeNode, nowSum: int):
+        if root is not None:
+            left = root.left is None
+            right = root.right is None
+            nowSum += root.val
+            if left and right:
+                if nowSum == self.totalSum:
+                    self.exist = True
+            self.checkSum(root.left, nowSum)
+            self.checkSum(root.right, nowSum)
