5c68ee38cd480f65cc8ad05313b8e62f490b9eaa
kkkkang1009 <kkkkkkkkang@nate.com>
2020-02-09 16:55:42 +0900

    Maximum Subarray

--- leetcode_53.py
+++ leetcode_53.py
@@ -1,16 +1,5 @@
 # 53. Maximum Subarray
-# Change scoring criteria - TimeOut
-# class Solution:
-#    def maxSubArray(self, nums: 'List[int]') -> 'int':
-#        max = nums[0]
-#        for i in range(0, len(nums)):
-#            sum = 0
-#            for j in range(i, len(nums)):
-#                sum += nums[j]
-#                if sum > max:
-#                    max = sum
-#        return max
-#
+
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         stack = []
@@ -21,3 +10,17 @@ class Solution:
             stack.append(i)
             list = max(list, sum(stack))
         return list
+
+# 1st TimeOut
+# class Solution:
+#    def maxSubArray(self, nums: 'List[int]') -> 'int':
+#        max = nums[0]
+#        for i in range(0, len(nums)):
+#            sum = 0
+#            for j in range(i, len(nums)):
+#                sum += nums[j]
+#                if sum > max:
+#                    max = sum
+#        return max
+
+#     Change scoring criteria
