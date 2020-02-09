6a83626ed5d2968730d55e9001556de27266d6d5
kkkkang1009 <kkkkkkkkang@nate.com>
2020-02-09 16:57:22 +0900

    Sqrt(x)

--- leetcode_69.py
+++ leetcode_69.py
@@ -1,5 +1,16 @@
 # 69. Sqrt(x)
-# Change scoring criteria - TimeOut
+
+class Solution:
+    def mySqrt(self, x: 'int') -> 'int':
+        answer = 0
+        while answer <=  x:
+            if answer*answer > x:
+                break
+            else:
+                answer += 1
+        return answer-1
+
+#1st - TimeOut
 # class Solution:
 #    def mySqrt(self, x: 'int') -> 'int':
 #        answer = 0
@@ -12,13 +23,4 @@
 #                answer += 1
 #        return answer-1
 
-
-class Solution:
-    def mySqrt(self, x: 'int') -> 'int':
-        answer = 0
-        while answer <=  x:
-            if answer*answer > x:
-                break
-            else:
-                answer += 1
-        return answer-1
+# Change scoring criteria
